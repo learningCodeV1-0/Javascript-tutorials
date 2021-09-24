0ffe78ee36fb06e90e363414ff4e48a406936556
shrwda <shrwda@gmail.com>
2021-09-24 18:11:48 +1000

    js variable and string

---
+++ Variable.js
@@ -0,0 +1,41 @@
+var myname = "Anju "; //var is  keyword; myname is name,  Anju  is value //
+
+
+prompt("what is your name?");
+
+var myName = "anju"; // input var name//
+alert(myName);
+
+var myName = prompt("what is your name?"); //imput tyour name//
+
+var myName = "Anju";
+var yourName = prompt("What is your name?");
+alert("My name is " + myName + ", Welcome to javascript "  + yourName + "!") // add vaeriable name to store data//
+
+
+
+var gameLevel = 1;
+gameLevel = 2;
+gameLevel = 3;
+alert("Your Level is " + gameLevel )
+
+
+//switch variables/
+
+function test();
+var a = 8;
+var b= 3;
+
+var c= a;
+a = b;
+b = c;
+
+
+console.log("a is " + a);
+console.log("b is " + b);
+
+
+
+
+
+
