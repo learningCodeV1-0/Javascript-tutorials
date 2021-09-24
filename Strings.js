9f6889c7a27bec413ab07058c86e7a9851220afe
shrwda <shrwda@gmail.com>
2021-09-24 18:00:09 +1000

    javascript var and string

---
+++ Strings.js
@@ -0,0 +1,23 @@
+var message = "hello";
+var myName = "Anju";
+
+alert(message +" " + "there" + " " +  myName);
+
+var myName= " Anju";
+myName.length;
+
+// create a tweet  number and count and the remaining 
+
+var tweet = prompt("compose your tweet");
+var tweetCount = tweet.length;
+alert("You have written" + tweetCount + " characters, you have" + (140 - tweetCount) + "character remaining.")
+
+
+
+//slice(x,y) rmove the word length 
+
+//start and end position starting from 0;
+//x is inclusive and y is exclusive 
+var myName = "Anju";
+myName.slice(1, 3);
+output= nj
