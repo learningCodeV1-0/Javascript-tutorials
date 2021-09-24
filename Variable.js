ab81c41d786315595e8baa19fa97f6278ed7983a
shrwda <shrwda@gmail.com>
2021-09-24 18:50:39 +1000

    resolved conflict

--- Variable.js
+++ Variable.js
@@ -12,20 +12,11 @@ var myName = "Anju";
 var yourName = prompt("What is your name?");
 alert("My name is " + myName + ", Welcome to javascript "  + yourName + "!") // add vaeriable name to store data//
 
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 0ffe78ee36fb06e90e363414ff4e48a406936556
 var gameLevel = 1;
 gameLevel = 2;
 gameLevel = 3;
 alert("Your Level is " + gameLevel )
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 0ffe78ee36fb06e90e363414ff4e48a406936556
 //switch variables/
 
 function test();
@@ -36,10 +27,6 @@ var c= a;
 a = b;
 b = c;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 0ffe78ee36fb06e90e363414ff4e48a406936556
 console.log("a is " + a);
 console.log("b is " + b);
 
